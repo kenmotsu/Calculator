300beb9564006bdde045962fde343ea4db0a4e2f
No 野中 亮 （ソフトウェア エンジニアリング技術１課） <nonaka@tmx.local>
2017-07-10 17:16:20 +0900

    initial commit

---
+++ calc/inc/base.h
@@ -0,0 +1,15 @@
+/*
+ * base.h
+ *
+ *  Created on: 2017/07/04
+ *      Author: fujisawa
+ */
+
+#ifndef BASE_H_
+#define BASE_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#endif /* BASE_H_ */
